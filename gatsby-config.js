6cda2db6aaba8ba29521ed5bdfbbb26b3cbb6a6b
Daan van der Zwaag <daan@devign.it>
2019-11-20 21:56:56 +0200

    Remove: pathfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,9 +16,8 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
     siteMetadata: {
-        title: "Gatsby Contentful starter",
+        title: "Daan van der Zwaag → Design Technologist",
     },
-    pathPrefix: "/gatsby-contentful-starter",
     plugins: [
         "gatsby-transformer-remark",
         "gatsby-transformer-sharp",
