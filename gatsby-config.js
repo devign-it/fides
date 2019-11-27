21516e4b9e762453fe13bdd4b0dbe53a0531d44f
Daan van der Zwaag <daan@devign.it>
2019-11-27 20:03:52 +0200

    Add: filesystem and manifest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,26 @@ module.exports = {
         "gatsby-plugin-sharp",
         "gatsby-plugin-sass",
         "gatsby-plugin-use-dark-mode",
+        "gatsby-plugin-offline",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: `${__dirname}/src/assets`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Daan van der Zwaag - Portfolio 2020`,
+                short_name: `Daan van der Zwaag`,
+                start_url: `/`,
+                background_color: `#000`,
+                theme_color: `#fff`,
+                display: `standalone`,
+                icon: `src/assets/images/favicons/favicon.png`,
+            },
+        },
         {
             resolve: "gatsby-source-contentful",
             options: contentfulConfig,
