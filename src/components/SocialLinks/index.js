a1c9d72821d7ac5749797722976114bd8457328b
Daan van der Zwaag <daan@devign.it>
2019-11-29 00:23:04 +0200

    Add: sociallinks component

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.scss";
 import ExternalLink from "../ExternalLink";
 
-const SocialLinks = ({ darkMode }) => {
+const SocialLinks = ({ darkMode, enableInternal }) => {
     let classNames = "social-link--wrapper";
 
     if (darkMode) {
@@ -11,18 +11,27 @@ const SocialLinks = ({ darkMode }) => {
     return (
         <>
             <div className={classNames}>
-                <ExternalLink text="Github" URL="https://github.com/danoszz" alignRight={true}></ExternalLink>
-                <ExternalLink text="Codepen" URL="https://codepen.io/danoszz/" alignRight={true}></ExternalLink>
-                <ExternalLink
-                    text="LinkedIn"
-                    URL="https://www.linkedin.com/in/daanvanderzwaag/"
-                    alignRight={true}
-                ></ExternalLink>
-                <ExternalLink
-                    text="Instagram"
-                    URL="https://www.instagram.com/daanvanderzwaag/"
-                    alignRight={true}
-                ></ExternalLink>
+                {enableInternal ? (
+                    <div className="internal--links">
+                        <ExternalLink
+                            URL="../resume-daanvanderzwaag_summer2019.pdf"
+                            text="Resume"
+                            internalPage={true}
+                            alignRight={true}
+                        />
+                        <ExternalLink URL="/projects/" text="Projects" internalPage={true} alignRight={true} />
+                    </div>
+                ) : null}
+                <div className="external--links">
+                    <ExternalLink text="Github" URL="https://github.com/danoszz" alignRight={true} />
+                    <ExternalLink text="Codepen" URL="https://codepen.io/danoszz/" alignRight={true} />
+                    <ExternalLink
+                        text="LinkedIn"
+                        URL="https://www.linkedin.com/in/daanvanderzwaag/"
+                        alignRight={true}
+                    />
+                    <ExternalLink text="Instagram" URL="https://www.instagram.com/daanvanderzwaag/" alignRight={true} />
+                </div>
             </div>
         </>
     );
