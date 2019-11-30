3cf43bed857d99ac08c4ea0d6647e71f4ec27f1a
Daan van der Zwaag <daan@devign.it>
2019-11-30 23:23:52 +0200

    Fix: added dark mode for TagsList

--- src/components/TagsList/index.js
+++ src/components/TagsList/index.js
@@ -3,22 +3,32 @@ import _ from "lodash";
 import { Link } from "gatsby";
 import "./styles.scss";
 
-export default ({ tagSource }) => (
-    <div className="tags--wrapper">
-        {tagSource.map(({ node }, i) => {
-            let tag = tagSource[i];
-            return (
-                // <Link key={tag.slug} className="categoryTag" to={`/tags/${_.kebabCase(tag.slug)}`}>
-                //     <span>
-                //         <p>{tag.category}</p>
-                //     </span>
-                // </Link>
-                <div key={tag.slug} className="categoryTag">
-                    <span>
-                        <p>{tag.category}</p>
-                    </span>
-                </div>
-            );
-        })}
-    </div>
-);
+const TagsList = ({ tagSource, showTagsBlack }) => {
+    let classNames = "categoryTag";
+
+    if (showTagsBlack) {
+        classNames += " black";
+    }
+
+    return (
+        <div className="tags--wrapper">
+            {tagSource.map(({ node }, i) => {
+                let tag = tagSource[i];
+                return (
+                    // <Link key={tag.slug} className="categoryTag" to={`/tags/${_.kebabCase(tag.slug)}`}>
+                    //     <span>
+                    //         <p>{tag.category}</p>
+                    //     </span>
+                    // </Link>
+                    <div key={tag.slug} className={classNames}>
+                        <span>
+                            <p>{tag.category}</p>
+                        </span>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default TagsList;
