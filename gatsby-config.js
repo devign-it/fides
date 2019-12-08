2cffc2c7ff9f8365990b1456edeb8ebe1fdb13c2
Daan van der Zwaag <daan@devign.it>
2019-12-08 23:53:17 +0100

    📦 Add and bump packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         "gatsby-plugin-sass",
         "gatsby-plugin-use-dark-mode",
         "gatsby-plugin-offline",
+        "gatsby-plugin-transition-link",
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
