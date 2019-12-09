b5ee56fff1086f7ca194e220036be0a62de7e1ef
Daan van der Zwaag <daan@devign.it>
2019-12-09 23:50:29 +0100

    Linter: fix issues

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -1,28 +1,24 @@
 let messagesGone = [
-  "Drinking beer, brb 🍺",
-  "Downloading virus ⏳",
-  "Open window alert 💨",
-  "Bye then 😌",
-  "Launch me again 🚀",
-  "See ya later 👋"
+    "Drinking beer, brb 🍺",
+    "Downloading virus ⏳",
+    "Open window alert 💨",
+    "Bye then 😌",
+    "Launch me again 🚀",
+    "See ya later 👋",
 ];
 let original;
 
 export function changeDocumentTitle() {
+    const windowIsFocused = document.hasFocus();
 
-  const windowIsFocused = document.hasFocus();
-  console.log('windowIsFocused', windowIsFocused)
-
-  if (windowIsFocused === true) {
-    document.title = original;
-  } else {
-    let title =  document.title;
-    console.log('title', title)
-    let choosenMessage =
-      messagesGone[Math.floor(Math.random() * messagesGone.length)];
-    if (title !== choosenMessage) {
-      original = title;
+    if (windowIsFocused === true) {
+        document.title = original;
+    } else {
+        let title = document.title;
+        let choosenMessage = messagesGone[Math.floor(Math.random() * messagesGone.length)];
+        if (title !== choosenMessage) {
+            original = title;
+        }
+        document.title = choosenMessage;
     }
-    document.title = choosenMessage;
-  }
 }
