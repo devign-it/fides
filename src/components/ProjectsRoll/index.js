4b3219b7d1a5c88617afd095d5aa9a6357802264
Daan van der Zwaag <daan@devign.it>
2020-01-09 17:42:13 +0100

    Feat: `ProjectsRoll` renders limited items

--- src/components/ProjectsRoll/index.js
+++ src/components/ProjectsRoll/index.js
@@ -1,18 +1,35 @@
 import React from "react";
+import { Link } from "gatsby";
 import ProjectPreview from "../ProjectPreview";
+import { ButtonWide } from "../Buttons";
 import "./styles.scss";
 
 class ProjectsRoll extends React.Component {
     render() {
+        let orginalProjects = this.props.items;
+        let limitedProjects = orginalProjects.slice(0, 4);
+        let mappedProjects = orginalProjects;
+
+        if (this.props.showButton) {
+            mappedProjects = limitedProjects;
+        }
+
         return (
             <section className="project-list">
-                {this.props.items.map(({ node }) => {
+                {mappedProjects.map(({ node }) => {
                     return (
                         <article className="project-item--wrapper" key={node.slug}>
                             <ProjectPreview project={node} />
                         </article>
                     );
                 })}
+                {this.props.showButton && (
+                    <ButtonWide
+                        inverted={false}
+                        text={"See all " + orginalProjects.length + " projects"}
+                        to="/projects"
+                    ></ButtonWide>
+                )}
             </section>
         );
     }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,6 +5,7 @@ import Helmet from "react-helmet";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import ProjectsRoll from "../components/ProjectsRoll";
+import "../styles/pages/projects.scss";
 
 class ProjectIndex extends React.Component {
     render() {
@@ -14,7 +15,7 @@ class ProjectIndex extends React.Component {
         return (
             <Layout location={this.props.location} showNav={true} showHome={true} pageName={"projects"}>
                 <Helmet title={siteTitle} />
-                <ProjectsRoll items={posts} />
+                <ProjectsRoll items={posts} showButton={false} amount={4} />
                 <Footer mode={"ghost"} showInternalLinks={false} />
             </Layout>
         );
@@ -25,7 +26,7 @@ export default ProjectIndex;
 
 export const pageQuery = graphql`
     query ProjectIndexQuery {
-        allContentfulProjectPosts(sort: { fields: order, order: ASC }, limit: 5) {
+        allContentfulProjectPosts(sort: { fields: order, order: ASC }) {
             edges {
                 node {
                     client
