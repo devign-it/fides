0b86ce07bc9522c4f2f51d3556a73c20d5cbc6df
Daan van der Zwaag <daan@devign.it>
2020-02-28 18:27:44 +0100

    hotfix: remove old import

--- src/components/CapabilityScroll/index.js
+++ src/components/CapabilityScroll/index.js
@@ -5,7 +5,6 @@ class CapabilityScroll extends React.Component {
     render() {
         let sortedListFirst = this.props.dataSourceFirst.sort();
         let sortedListSecond = this.props.dataSourceSecond.sort();
-        let sortedListThird = this.props.dataSourceThird.sort();
 
         return (
             <ul className="list--capabilities__horizontal">
@@ -18,10 +17,10 @@ class CapabilityScroll extends React.Component {
                 {sortedListSecond.map(({ node }, i) => {
                     return <li key={i}>{sortedListSecond[i]}</li>;
                 })}
-                <h3 className="list--title">{this.props.nameThird}</h3>
+                {/* <h3 className="list--title">{this.props.nameThird}</h3>
                 {sortedListThird.map(({ node }, i) => {
                     return <li key={i}>{sortedListFirst[i]}</li>;
-                })}
+                })} */}
             </ul>
         );
     }
